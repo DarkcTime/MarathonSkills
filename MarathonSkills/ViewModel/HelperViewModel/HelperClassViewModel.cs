5544966396e4ab30178523adfdaef691661d5b24
user1 <email>
2020-01-21 11:27:06 +0300

    Добавлены вспомогательные классы проекта

---
+++ MarathonSkills/ViewModel/HelperViewModel/HelperClassViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MarathonSkills.ViewModel.HelperViewModel
+{
+
+    public delegate void SetPageDelegate(Page page);
+
+    class HelperClassViewModel
+    {
+
+        public static SetPageDelegate setPage { get; set; }
+
+        public static void SetPage(Page page)
+        {
+            setPage?.Invoke(page);
+        }
+
+        public static void MessageBoxErrorStatic(Exception ex)
+        {
+            MessageBox.Show(ex.Message, ex.HelpLink, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        protected void MessageBoxError(Exception ex)
+        {
+            MessageBox.Show(ex.Message, ex.HelpLink, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+
+        protected void MessageBoxError(string message, string title = "Ошибка")
+        {
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        protected void MessageBoxInformation(string message, string title = "Успешно")
+        {
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        protected MessageBoxResult MessageBoxQuestion(string message, string title = "Вопрос")
+        {
+            MessageBoxResult result = MessageBox.Show(message, title, MessageBoxButton.YesNo, MessageBoxImage.Question);
+            return result;
+        }
+
+    }
+}

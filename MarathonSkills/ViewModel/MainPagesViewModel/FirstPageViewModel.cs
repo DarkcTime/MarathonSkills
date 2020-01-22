b0ac2a4b287786d700975b80058137a8cd127cea
vlad <vlad_chekmarev_01@mail.ru>
2020-01-22 12:04:31 +0300

    Изменена структура

---
+++ MarathonSkills/ViewModel/MainPagesViewModel/FirstPageViewModel.cs
@@ -0,0 +1,105 @@
+﻿using MarathonSkills.ViewModel.HelperViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace MarathonSkills.ViewModel.MainPagesViewModel
+{
+    class FirstPageViewModel : HelperViewModel.HelperClassViewModel
+    {
+
+        private string timeBeforeStart;
+
+        public string TimeBeforeStart
+        {
+            get => this.timeBeforeStart;
+            set => Set<string>(ref this.timeBeforeStart, value);
+        }
+
+        DispatcherTimer dispatcherTimer;
+
+
+
+        public ICommand RunnerCommand { get; set; }
+        public ICommand SponsorCommand { get; set; }
+        public ICommand InformationCommand { get; set; }
+        public ICommand LoginCommand { get; set; }
+
+
+
+        public FirstPageViewModel()
+        {
+
+            try
+            {
+
+
+                RunnerCommand = new Command(CommandRunnerClick);
+                this.SponsorCommand = new Command(CommandSponsorClick);
+                this.InformationCommand = new Command(CommandInformationClick);
+                this.LoginCommand = new Command(LoginCommandClick);
+
+
+                Classes.DateTimeMarathon dateTimeMarathon = new Classes.DateTimeMarathon();
+                this.TimeBeforeStart = dateTimeMarathon.GetDateTimeBeforeMarathone();
+
+                dispatcherTimer = new DispatcherTimer();
+                dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+                dispatcherTimer.Tick += timerTick;
+                dispatcherTimer.Start();
+
+            }
+            catch (Exception ex)
+            {
+                this.MessageBoxError(ex);
+            }
+
+        }
+
+
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            try
+            {
+                Classes.DateTimeMarathon dateTimeMarathon = new Classes.DateTimeMarathon();
+                this.TimeBeforeStart = dateTimeMarathon.GetDateTimeBeforeMarathone();
+            }
+            catch (Exception ex)
+            {
+                this.MessageBoxError(ex);
+            }
+        }
+
+
+
+
+        #region Обработчики кнопок
+
+        private void LoginCommandClick(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CommandInformationClick(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CommandRunnerClick(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CommandSponsorClick(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
--- MarathonSkills/ViewModel/MainWinodowSystemViewModel/MainWindowSystemViewModel.cs
+++ MarathonSkills/ViewModel/MainWinodowSystemViewModel/MainWindowSystemViewModel.cs
@@ -15,87 +15,10 @@ namespace MarathonSkills.ViewModel.MainWinodowSystemViewModel
     {
         public static Action CloseWindow { get; set; }
 
-        private string timeBeforeStart;
-        
-        public string TimeBeforeStart
-        {
-            get => this.timeBeforeStart;
-            set => Set<string>(ref this.timeBeforeStart , value); 
-        }
+       public MainWindowSystemViewModel()
+       {
 
-        DispatcherTimer dispatcherTimer;
+       }
 
-   
-
-        public ICommand RunnerCommand { get; set; }
-        public ICommand SponsorCommand { get; set; }
-        public ICommand InformationCommand { get; set; }
-        public ICommand LoginCommand { get; set; }
-
-        public MainWindowSystemViewModel()
-        {
-
-            try
-            {
-                Classes.DateTimeMarathon dateTimeMarathon = new Classes.DateTimeMarathon();
-                this.TimeBeforeStart = dateTimeMarathon.GetDateTimeBeforeMarathone();
-
-
-                RunnerCommand = new Command(CommandRunnerClick);
-                this.SponsorCommand = new Command(CommandSponsorClick);
-                this.InformationCommand = new Command(CommandInformationClick);
-                this.LoginCommand = new Command(LoginCommandClick);
-
-                dispatcherTimer = new DispatcherTimer();
-                dispatcherTimer.Interval = new TimeSpan(0,0,1);
-                dispatcherTimer.Tick += timerTick;
-                dispatcherTimer.Start(); 
-            }
-            catch (Exception ex)
-            {
-                this.MessageBoxError(ex);
-            }
-
-        }
-
-        private void timerTick(object sender, EventArgs e)
-        {
-            try
-            {
-                Classes.DateTimeMarathon dateTimeMarathon = new Classes.DateTimeMarathon();
-                this.TimeBeforeStart = dateTimeMarathon.GetDateTimeBeforeMarathone();
-            }
-            catch (Exception ex)
-            {
-                this.MessageBoxError(ex);
-            }
-        }
-
-        
-
-
-        #region Обработчики кнопок
-
-        private void LoginCommandClick(object obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void CommandInformationClick(object obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void CommandRunnerClick(object obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void CommandSponsorClick(object obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        #endregion
     }
 }

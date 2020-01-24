e00b3e55d3b75373187695a7d03f08e5b31a2d06
vlad <vlad_chekmarev_01@mail.ru>
2020-01-24 08:19:03 +0300

    Добавьте файлы проекта.

---
+++ MarathonSkills/ViewModel/HelperViewModel/HelperClassViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MarathonSkills.ViewModel.HelperViewModel
+{
+
+    public delegate void SetPageDelegate(Page page);
+
+
+    class HelperClassViewModel : ViewModelProp
+    {
+
+        public static SetPageDelegate setPage { get; set; }
+
+        public static SetPageDelegate setPageSecondPage { get; set; }
+
+        public static void SetPage(Page page)
+        {
+            setPage?.Invoke(page);
+        }
+
+        public static void SetPageSecondPage(Page page)
+        {
+            setPageSecondPage?.Invoke(page);
+        }
+
+
+        protected Model.user25Entities context = new Model.user25Entities(); 
+
+        public static void MessageBoxErrorStatic(Exception ex)
+        {
+            MessageBox.Show(ex.Message, ex.HelpLink, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        protected void MessageBoxError(Exception ex)
+        {
+            MessageBox.Show(ex.Message, ex.HelpLink, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+
+        protected void MessageBoxError(string message, string title = "Ошибка")
+        {
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        protected void MessageBoxWarning(string message, string title = "Проверьте данные")
+        {
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+
+        protected void MessageBoxInformation(string message, string title = "Успешно")
+        {
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        protected MessageBoxResult MessageBoxQuestion(string message, string title = "Вопрос")
+        {
+            MessageBoxResult result = MessageBox.Show(message, title, MessageBoxButton.YesNo, MessageBoxImage.Question);
+            return result;
+        }
+
+    }
+}
---
+++ MarathonSkills/ViewModel/MainWinodowSystemViewModel/MainWindowSystemViewModel.cs
@@ -0,0 +1,70 @@
+﻿using MarathonSkills.ViewModel.HelperViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace MarathonSkills.ViewModel.MainWinodowSystemViewModel
+{
+    class MainWindowSystemViewModel : HelperViewModel.HelperClassViewModel
+    {
+        public static Action CloseWindow { get; set; }
+
+        private string title;
+        public string Title
+        {
+            get => this.title;
+            set => Set<string>(ref title, value);
+        }
+
+        private Page currentPage;
+
+        public Page CurrentPage
+        {
+
+            get => this.currentPage;
+            set => this.Set<Page>(ref this.currentPage, value);
+        }
+
+
+
+        public MainWindowSystemViewModel()
+       {
+            try
+            {
+                setPage = SetPages;
+
+                SetPage(new View.MainPages.FirstPage());
+
+            }
+            catch (Exception ex)
+            {
+                this.MessageBoxError(ex);
+            }
+
+       }
+
+
+        private void SetPages(Page page)
+        {
+
+            try
+            {
+
+                CurrentPage = page;
+                Title = page.Title;
+            }
+            catch (Exception ex)
+            {
+                this.MessageBoxError(ex);
+            }
+
+        }
+
+    }
+}
